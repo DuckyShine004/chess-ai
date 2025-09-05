547fe071793593e35a3071689577c1d96c48b78f
Gallon Zhou <gallonzhou04@gmail.com>
2025-09-06 04:12:31 +1200

    Feat: Added Queen Attacks

---
+++ include/engine/piece/Queen.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "engine/piece/Bishop.hpp"
+#include "engine/piece/Rook.hpp"
+
+namespace engine::piece::Queen {
+
+[[nodiscard]] inline constexpr uint64_t getAttacks(int square, uint64_t occupancy);
+
+[[nodiscard]] inline constexpr uint64_t getAttacks(int square, uint64_t occupancy) {
+    return Bishop::getAttacks(square, occupancy) | Rook::getAttacks(square, occupancy);
+}
+
+} // namespace engine::piece::Queen
